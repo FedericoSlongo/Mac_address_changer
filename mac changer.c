a7cc4dee3ca3bdff9926682e17b9f3e873967560
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-12-21 17:04:08 +0100

    The mac address gets changed

--- mac changer.c
+++ mac changer.c
@@ -18,7 +18,7 @@ int main(){
     gets(adaptor);
 
     //Checking if the mac address is a valid one
-    if(mac[2] != ':'){
+    if(mac[2] != ':' || mac[5] != ':' || mac[8] != ':' || mac[11] != ':' || mac[14] != ':'){
         printf("The mac address is not valid");
         return 2;
     }
@@ -38,7 +38,25 @@ int main(){
     command[temp + 3] = 'n';
     command[temp + 4] = '\0';
 
+    //The adaptor gets put down
     printf("The adaptor is being put down\n");
-    system(command);
+    //system(command);
     printf("The adaptor is down\n");
+
+    //The adpator mac gets changed 
+    command[temp] = 'a';
+    command[temp + 1] = 'd';
+    command[temp + 2] = 'd';
+    command[temp + 3] = 'r';
+    command[temp + 4] = 'e';
+    command[temp + 5] = 's';
+    command[temp + 6] = 's';
+    command[temp + 7] = ' ';
+    for (int i = 0; i < 18; i++)
+        command[(temp + 7) + (i + 1)] = mac[i];
+
+    printf("The mac address is being changed\n");
+    //system(command);
+    printf("The mac address is changed\n")
+    
 }
